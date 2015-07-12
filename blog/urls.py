ae9fed43e48e76daff97274660e16d961849deb0
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-12 20:01:24 +0100

    Add about page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 
 urlpatterns = [
+    url(r'^about/*$', 'blog.views.about', name='about'),
+
     url(r'^(?P<slug>[\w-]+)/*$', 'blog.views.post', name='post'),
     url(r'^$', 'blog.views.home', name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 
 from blog.models import Post
 
@@ -17,3 +17,9 @@ class PostView(DetailView):
     slug_field = 'slug'
 
 post = PostView.as_view()
+
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+about = AboutView.as_view()
