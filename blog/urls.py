70737e8b7cc50051a28b55cc69597e456b4661ab
Javi Manzano <javi.manzano.oller@gmail.com>
2015-07-14 20:00:43 +0100

    Fix all urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^about/*$', 'blog.views.about', name='about'),
 
-    url(r'^(?P<slug>[\w-]+)/*$', 'blog.views.post', name='post'),
+    url(r'^p/(?P<slug>[\w-]+)/*$', 'blog.views.post', name='post'),
     url(r'^$', 'blog.views.home', name='home'),
 ]
