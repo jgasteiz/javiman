77e3bf7f37954fda3f93593bfc889cd73da0a174
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-17 19:05:16 +0100

    Add PhotoForm

--- cms/forms.py
+++ cms/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from blog.models import Post
+from blog.models import Post, Photo
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         fields = ('title', 'slug', 'body', 'is_published', 'created')
         model = Post
+
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        fields = ('title', 'subtitle', 'url', 'is_published')
+        model = Photo
