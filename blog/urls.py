678b3c8d6cac470bb604eb6a06762bdf41ef89ce
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-17 19:15:16 +0100

    Show photos instead of flickr stuff in the public photos page

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^about/$', 'blog.views.about', name='about'),
-    url(r'^albums/$', 'blog.views.album_list', name='album_list'),
-    # url(r'^albums/(?P<slug>[\w-]+)/$', 'blog.views.album_detail', name='album_detail'),
+    url(r'^photos/$', 'blog.views.photos', name='photos'),
 
     url(r'^p/(?P<slug>[\w-]+)/$', 'blog.views.post', name='post'),
     url(r'^$', 'blog.views.home', name='home'),
