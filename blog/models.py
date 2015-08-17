64693f6869ccca22ab755a50c420a4b7640da75f
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-17 19:04:41 +0100

    Add Photo and PhotoManager models

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,39 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-created',)
+
+
+class PhotoManager(models.Manager):
+    def get_queryset(self):
+        return super(PhotoManager, self).get_queryset()
+
+    def published(self):
+        return super(PhotoManager, self).get_queryset().filter(is_published=True)
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=256, blank=True)
+    subtitle = models.CharField(max_length=256, blank=True)
+    url = models.CharField(max_length=256)
+    is_published = models.BooleanField(default=False)
+    order = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    objects = PhotoManager()
+
+    class Meta:
+        ordering = ('order', '-created',)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_flickr_preview(self):
+        """
+        This will only work for jpeg images stored in flickr.
+        Given a url like:
+            //farm1.staticflickr.com/702/20638346971_1c14fc9bff_l.jpg,
+        this will return something like:
+            //farm1.staticflickr.com/702/20638346971_1c14fc9bff_m.jpg
+        """
+        image_url = self.url[:-6]
+        return '{}_m.jpg'.format(image_url)
