acad39fbff00c141b1fca139b0e9a2ed67e2feff
Javi Manzano <javi.manzano.oller@gmail.com>
2015-08-17 19:05:02 +0100

    Test for Photo model

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Photo
+
+
+class PhotoTestCase(TestCase):
+    def setUp(self):
+        self.photo = Photo(
+            title='Stained Glass',
+            subtitle='Canterbury Cathedral',
+            is_published=True,
+            url='https://farm1.staticflickr.com/431/19871282374_102b88bb01_b.jpg'
+        )
+
+    def test_get_flickr_preview(self):
+        preview_url = 'https://farm1.staticflickr.com/431/19871282374_102b88bb01_m.jpg'
+        self.assertEqual(self.photo.get_flickr_preview(), preview_url)
